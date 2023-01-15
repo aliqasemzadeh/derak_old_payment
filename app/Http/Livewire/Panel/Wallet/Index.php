4726af0bcb7124f1caffc53b438becc009902a4b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-01-15 09:31:26 +0330

    update

--- app/Http/Livewire/Panel/Wallet/Index.php
+++ app/Http/Livewire/Panel/Wallet/Index.php
@@ -6,6 +6,19 @@ use Livewire\Component;
 
 class Index extends Component
 {
+    use WithPagination;
+    use LivewireAlert;
+
+    public $selectedItems = [];
+    public $selectAll = false;
+
+    public $terminal;
+    public $search;
+    public $perPage = 15;
+    public $sortColumn = 'updated_at';
+    public $sortDirection = 'desc';
+
+    protected $paginationTheme = 'bootstrap';
     public function render()
     {
         return view('livewire.panel.wallet.index')->layout('layouts.panel');
