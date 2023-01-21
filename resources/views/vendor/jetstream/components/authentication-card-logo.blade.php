8c4d44bb6a7a1b22fa987e868004d242d97dbece
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-01-21 09:50:55 +0330

    update

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,3 +1,4 @@
 <a class="d-flex justify-content-center mb-4" href="{{ route('home') }}">
     <img width="128" src="{{ asset('images/logo-dark.svg') }}" {{ $attributes }} />
+    <img width="128" src="{{ asset('images/logo-dark.svg') }}" {{ $attributes }} />
 </a>
