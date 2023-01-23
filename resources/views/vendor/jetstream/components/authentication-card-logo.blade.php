74b28e3ee615d8ffeec3a90a59c4bd594da5c692
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-01-23 07:38:53 +0330

    dark logo update

--- resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
+++ resources/views/vendor/jetstream/components/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
 <a class="d-flex justify-content-center mb-4" href="{{ route('home') }}">
-    <img x-show="!darkTheme" width="128" src="{{ asset('images/logo-light.svg') }}" {{ $attributes }} />
-    <img x-show="darkTheme" width="128" src="{{ asset('images/logo-dark.svg') }}" {{ $attributes }} />
+    <img x-show="darkTheme" width="128" src="{{ asset('images/logo-light.svg') }}" {{ $attributes }} />
+    <img x-show="!darkTheme" width="128" src="{{ asset('images/logo-dark.svg') }}" {{ $attributes }} />
 </a>
