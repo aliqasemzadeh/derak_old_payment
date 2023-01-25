78a333e84dcece17529cca89fccf645d28bb80c7
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-01-25 09:19:40 +0330

    dark logo update

--- resources/views/vendor/jetstream/components/application-logo.blade.php
+++ resources/views/vendor/jetstream/components/application-logo.blade.php
@@ -1,1 +1,2 @@
-<img src="{{ asset('images/logo-light.svg') }}" {{ $attributes }} />
+<img x-show="darkTheme" src="{{ asset('images/logo-light.svg') }}" {{ $attributes }} />
+<img x-show="!darkTheme" src="{{ asset('images/logo-dark.svg') }}" {{ $attributes }} />
