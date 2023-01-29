eea6f6cdf5e9b649dbb90a96c50e23224ccc1393
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-01-29 15:43:01 +0330

    update invoicing.

--- app/Http/Livewire/Panel/Invoice/Create.php
+++ app/Http/Livewire/Panel/Invoice/Create.php
@@ -2,12 +2,29 @@
 
 namespace App\Http\Livewire\Panel\Invoice;
 
+use App\Models\Invoice;
 use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
 
 class Create extends Component
 {
     use LivewireAlert;
+
+    public float $total;
+    public string $description;
+
+    public function create() : void
+    {
+        $this->validate([
+            'total' => 'required|numeric',
+            'description' => 'required|string',
+        ]);
+
+        $invoice = new Invoice();
+
+        $invoice->save();
+    }
+
     public function render()
     {
         return view('livewire.panel.invoice.create');
