ef00d673c05e4e7553f7c4da8f826322e1dc26ef
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-01-31 08:15:21 +0330

    update store system.

---
+++ app/Http/Livewire/Panel/Store/Create.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Panel\Store;
+
+use Livewire\Component;
+
+class Create extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.store.create');
+    }
+}
---
+++ app/Http/Livewire/Panel/Store/Edit.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Panel\Store;
+
+use Livewire\Component;
+
+class Edit extends Component
+{
+    public function render()
+    {
+        return view('livewire.panel.store.edit');
+    }
+}
