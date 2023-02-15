c3348e5cc48d2dfa55273514f892cfc70f15610c
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-02-15 16:31:07 +0330

    Add director links.

---
+++ app/Http/Livewire/Director/User/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Director\User;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.director.user.index');
+    }
+}
