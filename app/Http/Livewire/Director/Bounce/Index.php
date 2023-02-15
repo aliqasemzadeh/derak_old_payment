6a27a6428ee08e3b5748c632539b0293aaf8e86e
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-02-15 16:52:41 +0330

    Add sidebar links.

---
+++ app/Http/Livewire/Director/Bounce/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Director\Bounce;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.director.bounce.index');
+    }
+}
---
+++ app/Http/Livewire/Director/Level/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Director\Level;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.director.level.index');
+    }
+}
