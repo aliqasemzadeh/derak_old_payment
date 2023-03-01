5badeac6ba0c4af41a78796b73f6cf00c4e89aeb
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-01 20:12:37 +0330

    update links.

--- resources/views/livewire/director/user/wallet/transactions.blade.php
+++ resources/views/livewire/director/user/wallet/transactions.blade.php
@@ -6,8 +6,8 @@
         <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
             <li class="breadcrumb-item"><a href="{{ route('director.dashboard.index') }}">{{ __('bap.dashboard') }}</a></li>
             <li class="breadcrumb-item"><a href="{{ route('director.financial.index') }}">{{ __('bap.financial') }}</a></li>
-            <li class="breadcrumb-item"><a href="{{ route('director.financial.symbol.index') }}">{{ __('bap.symbols') }}</a></li>
-            <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('director.financial.symbol.transactions', [$symbol->symbol]) }}">{{ $symbol->symbol }}</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('director.exchange.symbol.index') }}">{{ __('bap.symbols') }}</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('director.exchange.symbol.transactions', [$symbol->symbol]) }}">{{ $symbol->symbol }}</a></li>
         </ol>
     </x-slot>
 
