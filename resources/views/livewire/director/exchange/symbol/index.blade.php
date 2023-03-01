b76a63bb3607366a0844b6c1320f7c568224325f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-01 20:17:33 +0330

    update links.

--- resources/views/livewire/director/exchange/symbol/index.blade.php
+++ resources/views/livewire/director/exchange/symbol/index.blade.php
@@ -91,7 +91,7 @@
                             </div>
                         </td>
                         <td class="text-end">
-                            <button onclick="Livewire.emit('showModal', 'director.financial.symbol.edit', '{{ json_encode($symbol->id) }}')" class="btn btn-primary btn-icon btn-sm">
+                            <button onclick="Livewire.emit('showModal', 'director.exchange.symbol.edit', '{{ json_encode($symbol->id) }}')" class="btn btn-primary btn-icon btn-sm">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /><path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /><line x1="16" y1="5" x2="19" y2="8" /></svg>
                             </button>
 
@@ -99,7 +99,7 @@
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="4" y1="7" x2="20" y2="7" /><line x1="10" y1="11" x2="10" y2="17" /><line x1="14" y1="11" x2="14" y2="17" /><path d="M5 7l1 12a2 2 0 0 0 2 2h8a2 2 0 0 0 2 -2l1 -12" /><path d="M9 7v-3a1 1 0 0 1 1 -1h4a1 1 0 0 1 1 1v3" /></svg>
                             </button>
 
-                            <a href="{{  route('director.financial.symbol.transactions', [$symbol->id]) }}" class="btn btn-orange btn-icon btn-sm">
+                            <a href="{{  route('director.exchange.symbol.transactions', [$symbol->id]) }}" class="btn btn-orange btn-icon btn-sm">
                                 <!-- Download SVG icon from http://tabler-icons.io/i/list -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><line x1="9" y1="6" x2="20" y2="6" /><line x1="9" y1="12" x2="20" y2="12" /><line x1="9" y1="18" x2="20" y2="18" /><line x1="5" y1="6" x2="5" y2="6.01" /><line x1="5" y1="12" x2="5" y2="12.01" /><line x1="5" y1="18" x2="5" y2="18.01" /></svg>
                             </a>
