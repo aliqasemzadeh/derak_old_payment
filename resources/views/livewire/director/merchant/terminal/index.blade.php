9e327412b3086104fec9877c647013208a36e93f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-08 10:01:24 +0330

    list transactions.

--- resources/views/livewire/director/merchant/terminal/index.blade.php
+++ resources/views/livewire/director/merchant/terminal/index.blade.php
@@ -189,12 +189,15 @@
                                 </button>
                             @endif
 
-                            <a href="{{ route('director.merchant.terminal.transactions', [$terminal->id]) }}" class="btn btn-dark btn-icon btn-sm">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-terminal-2" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                            <a href="{{ route('director.merchant.terminal.transactions', [$terminal->id]) }}" class="btn btn-success btn-icon btn-sm">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-list" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                                    <path d="M8 9l3 3l-3 3"></path>
-                                    <path d="M13 15l3 0"></path>
-                                    <path d="M3 4m0 2a2 2 0 0 1 2 -2h14a2 2 0 0 1 2 2v12a2 2 0 0 1 -2 2h-14a2 2 0 0 1 -2 -2z"></path>
+                                    <path d="M9 6l11 0"></path>
+                                    <path d="M9 12l11 0"></path>
+                                    <path d="M9 18l11 0"></path>
+                                    <path d="M5 6l0 .01"></path>
+                                    <path d="M5 12l0 .01"></path>
+                                    <path d="M5 18l0 .01"></path>
                                 </svg>
                             </a>
 
