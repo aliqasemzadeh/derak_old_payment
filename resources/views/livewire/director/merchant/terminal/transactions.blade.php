6d919d7b8ecc268307c97703db298bc5e18041cf
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-08 10:38:16 +0330

    list transactions.

--- resources/views/livewire/director/merchant/terminal/transactions.blade.php
+++ resources/views/livewire/director/merchant/terminal/transactions.blade.php
@@ -1,3 +1,16 @@
 <div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+    <x-slot name="title">
+        {{ $terminal->merchant->title }} - {{ __('bap.terminals') }} - {{ __('bap.transactions') }}
+    </x-slot>
+
+    <x-slot name="breadcrumb">
+        <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
+            <li class="breadcrumb-item"><a href="{{ route('director.dashboard.index') }}">{{ __('bap.dashboard') }}</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('director.merchant.index') }}">{{ __('bap.merchants') }}</a></li>
+            <li class="breadcrumb-item"><a href="{{ route('director.merchant.terminal.index', [$terminal->merchant->id ]) }}">{{ $terminal->merchant->title }} - {{ __('bap.terminals') }}</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('director.merchant.terminal.index', [$terminal->merchant->id]) }}">{{ $terminal->merchant->title }} - {{ __('bap.terminals') }}</a></li>
+        </ol>
+    </x-slot>
+
+
 </div>
