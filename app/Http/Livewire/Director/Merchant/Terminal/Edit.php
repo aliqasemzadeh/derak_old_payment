6a3d9bc93512bff0bb95a591f89ba6cfdb0d84a4
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-10 11:22:28 +0330

    list transactions.

--- app/Http/Livewire/Director/Merchant/Terminal/Edit.php
+++ app/Http/Livewire/Director/Merchant/Terminal/Edit.php
@@ -2,10 +2,34 @@
 
 namespace App\Http\Livewire\Director\Merchant\Terminal;
 
+use App\Models\Merchant;
+use App\Models\Terminal;
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use Livewire\WithFileUploads;
 
 class Edit extends Component
 {
+    use LivewireAlert;
+    use WithFileUploads;
+    public Terminal $Terminal;
+    public Merchant $merchant;
+    public $type = 'crypto';
+    public $title;
+    public $username;
+    public $callback_password;
+    public $callback_url;
+
+    public function mount(Terminal $terminal)
+    {
+        $this->terminal = $terminal;
+        $this->type = $terminal->type;
+        $this->title = $terminal->title;
+        $this->username = $terminal->username;
+        $this->callback_password = $terminal->callback_password;
+        $this->callback_url = $terminal->callback_url;
+    }
+
     public function render()
     {
         return view('livewire.director.merchant.terminal.edit');
--- resources/views/livewire/director/merchant/terminal/edit.blade.php
+++ resources/views/livewire/director/merchant/terminal/edit.blade.php
@@ -1,8 +1,8 @@
 <div class="modal-dialog modal-lg">
-    <form wire:submit.prevent="create">
+    <form wire:submit.prevent="edit">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">{{ __('bap.create_terminal') }}: {{ $merchant->title }}</h5>
+                <h5 class="modal-title">{{ __('bap.edit_terminal') }}: {{ $terminal->merchant->title }} {{ $terminal->title }}</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="{{ __('bap.close') }}"></button>
             </div>
             <div class="modal-body">
