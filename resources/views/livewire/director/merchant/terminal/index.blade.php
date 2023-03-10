c2ef88960a3a06e06d88d4adfcdacfa0755c2113
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-10 10:37:51 +0330

    list transactions.

--- resources/views/livewire/director/merchant/terminal/index.blade.php
+++ resources/views/livewire/director/merchant/terminal/index.blade.php
@@ -187,13 +187,13 @@
                             {{ $terminal->created_at }}
                         </td>
                         <td class="text-end">
-                            <button onclick="Livewire.emit('showModal', 'director.merchant.edit', '{{ json_encode($terminal->id) }}')" class="btn btn-primary btn-icon btn-sm">
+                            <button onclick="Livewire.emit('showModal', 'director.merchant.terminal.edit', '{{ json_encode($terminal->id) }}')" class="btn btn-primary btn-icon btn-sm">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M9 7h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" /><path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" /><line x1="16" y1="5" x2="19" y2="8" /></svg>
                             </button>
 
 
                             @if($merchant->status != 'enable')
-                                <button onclick="Livewire.emit('showModal', 'director.merchant.verify', '{{ json_encode($terminal->id) }}')" class="btn btn-warning btn-icon btn-sm">
+                                <button onclick="Livewire.emit('showModal', 'director.merchant.terminal.verify', '{{ json_encode($terminal->id) }}')" class="btn btn-warning btn-icon btn-sm">
                                     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-zoom-check" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                                         <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                                         <path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0"></path>
