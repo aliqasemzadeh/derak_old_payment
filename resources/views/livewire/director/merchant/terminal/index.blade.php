29047d6aab74214f431a2d64db1cd6d3ef805ce8
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-12 10:16:14 +0330

    copy api key

--- resources/views/livewire/director/merchant/terminal/index.blade.php
+++ resources/views/livewire/director/merchant/terminal/index.blade.php
@@ -180,8 +180,15 @@
                         <td>
                             {{ __('bap.'.$terminal->type) }}
                         </td>
-                        <td>
+                        <td x-data="{ api_key: '{{ $terminal->api_key }}' }">
                             {{ $terminal->api_key }}
+                            <button class="btn-action" x-clipboard="api_key">
+                                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-copy" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                                    <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                                    <path d="M8 8m0 2a2 2 0 0 1 2 -2h8a2 2 0 0 1 2 2v8a2 2 0 0 1 -2 2h-8a2 2 0 0 1 -2 -2z"></path>
+                                    <path d="M16 8v-2a2 2 0 0 0 -2 -2h-8a2 2 0 0 0 -2 2v8a2 2 0 0 0 2 2h2"></path>
+                                </svg>
+                            </button>
                         </td>
                         <td>
                             {{ $terminal->created_at }}
