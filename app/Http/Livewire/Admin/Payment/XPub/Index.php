47370fe1577dcfdbb4862ddd8699ff2a33ce13be
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-19 10:52:42 +0330

    xpub index making.

--- app/Http/Livewire/Admin/Payment/XPub/Index.php
+++ app/Http/Livewire/Admin/Payment/XPub/Index.php
@@ -2,10 +2,33 @@
 
 namespace App\Http\Livewire\Admin\Payment\XPub;
 
+use Jantinnerezo\LivewireAlert\LivewireAlert;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class Index extends Component
 {
+    use WithPagination;
+    use LivewireAlert;
+    public $selectedItems = [];
+    public $selectAll = false;
+
+    public $xpub;
+    public $search;
+    public $perPage = 15;
+    public $sortColumn = 'created_at';
+    public $sortDirection = 'asc';
+
+    protected $paginationTheme = 'bootstrap';
+
+    protected $queryString = ['search'];
+
+    protected $listeners = [
+        'confirmedDelete',
+        'cancelledDelete',
+        'deleteSelectedQuery',
+        'updateList' => 'render'
+    ];
     public function render()
     {
         return view('livewire.admin.payment.x-pub.index');
