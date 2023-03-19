863659d86e4581323efe288db5c30fbb29b6644a
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-19 12:03:29 +0330

    Add getPrice method to get rate of each symbol.

--- app/Models/Rate.php
+++ app/Models/Rate.php
@@ -10,4 +10,23 @@ class Rate extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public static function getPrice($symbol)
+    {
+        if (Cache::has($symbol.'_rate')) {
+            return Cache::get($symbol.'_rate');
+        } else {
+            if(isset(\App\Models\Rate::where('symbol', $symbol)->latest()->first()->price)) {
+                $price = (float) \App\Models\Rate::where('symbol', $symbol)->latest()->first()->price;
+                if(round($price, 8, PHP_ROUND_HALF_DOWN) == 0) {
+                    Cache::put($symbol.'_rate', "N/A");
+                } else {
+                    Cache::put($symbol.'_rate', rtrim(rtrim(sprintf("%10.8f", $price), '0'), '.'));
+                }
+
+            } else {
+                Cache::put($symbol.'_rate', "N/A");
+            }
+        }
+    }
 }
