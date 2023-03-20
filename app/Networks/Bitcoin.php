0edeb55d892f0aebc2ed7e21246323c5df4d2d2f
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-20 11:49:13 +0330

    Address Assignment.

--- app/Networks/Bitcoin.php
+++ app/Networks/Bitcoin.php
@@ -2,12 +2,19 @@
 
 namespace App\Networks;
 
+use App\Models\Address;
 use App\Models\Invoice;
 
 class Bitcoin
 {
-    public static function getInvoiceAddress(Invoice $invoice)
+    public static function getInvoiceAddress(Invoice $invoice, $symbol) : Address
     {
-
+        $address = Address::unused()->ofNetwork('BTC')->latest()->first();
+        $address->symbol = $symbol;
+        $address->user_id = $invoice->user_id;
+        $address->terminal_id = $invoice->terminal_id;
+        $address->invoice_id = $invoice->invoice_id;
+        $address->status = 'used';
+        return $address;
     }
 }
