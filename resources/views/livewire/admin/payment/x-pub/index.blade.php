15e90c9792de00bdbc74ea6327fb061f850e02ea
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-20 08:57:38 +0330

    add xpub buttons.

--- resources/views/livewire/admin/payment/x-pub/index.blade.php
+++ resources/views/livewire/admin/payment/x-pub/index.blade.php
@@ -2,6 +2,19 @@
     <x-slot name="title">
         {{ __('bap.xpubs') }}
     </x-slot>
+    <x-slot name="actions">
+        <div class="col-auto ms-auto d-print-none">
+            <div class="btn-list">
+                <button onclick="Livewire.emit('showModal', 'admin.payment.x-pub.create')" class="btn btn-primary d-none d-sm-inline-block">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                    {{ __('bap.create_xpub') }}
+                </button>
+                <button onclick="Livewire.emit('showModal', 'admin.payment.x-pub.create')" class="btn btn-primary d-sm-none btn-icon" aria-label="{{ __('bap.create_xpub') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
+                </button>
+            </div>
+        </div>
+    </x-slot>
     <x-slot name="breadcrumb">
         <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.index') }}">{{ __('bap.dashboard') }}</a></li>
