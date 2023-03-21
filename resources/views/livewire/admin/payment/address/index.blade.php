9f97ff1ef358bb9455f19f254712163f9a7d1f92
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-21 12:08:24 +0330

    edit balance in list

--- resources/views/livewire/admin/payment/address/index.blade.php
+++ resources/views/livewire/admin/payment/address/index.blade.php
@@ -75,9 +75,9 @@
                             @endif
                         @endif
                     </th>
-                    <th wire:click="sortByColumn('symbol')">{{ __('bap.symbol') }}
+                    <th wire:click="sortByColumn('network')">{{ __('bap.network') }}
 
-                        @if ($sortColumn == 'symbol')
+                        @if ($sortColumn == 'network')
                             @if($sortDirection == 'asc')
                                 <!-- Download SVG icon from http://tabler-icons.io/i/chevron-up -->
                                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-sm text-dark icon-thick" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><polyline points="6 15 12 9 18 15" /></svg>
@@ -137,14 +137,14 @@
                         <td>{{ $address->id }}</td>
                         <td>
                             <div class="d-flex py-1 align-items-center">
-                                <span class="avatar me-2" style="background-image: url()"></span>
+                                <span class="avatar me-2" style="background-image: url('{{ asset('cryptocurrency-icons/'.strtolower($address->symbol).'.svg') }}')"></span>
                                 <div class="flex-fill">
                                     <div class="font-weight-medium">{{ $address->address }}</div>
 
                                 </div>
                             </div>
                         </td>
-                        <td>{{ $address->symbol }}</td>
+                        <td>{{ $address->network }}</td>
                         <td>{{ $address->status }}</td>
                         <td>{{ $address->balance }}</td>
                         <td>{{ $address->created_at }}</td>
