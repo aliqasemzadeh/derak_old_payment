7459714d681bcf38b55b424a2e7d0585bca308e6
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-03-26 08:26:11 +0330

    fix name

--- resources/views/livewire/admin/payment/address/index.blade.php
+++ resources/views/livewire/admin/payment/address/index.blade.php
@@ -5,7 +5,7 @@
     <x-slot name="breadcrumb">
         <ol class="breadcrumb breadcrumb-arrows" aria-label="breadcrumbs">
             <li class="breadcrumb-item"><a href="{{ route('admin.dashboard.index') }}">{{ __('bap.dashboard') }}</a></li>
-            <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('admin.payment.x-pub.index') }}">{{ __('bap.xpubs') }}</a></li>
+            <li class="breadcrumb-item active" aria-current="page"><a href="{{ route('admin.payment.x-pub.index') }}">{{ __('bap.addresses') }}</a></li>
         </ol>
     </x-slot>
 
