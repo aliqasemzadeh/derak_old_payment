01426718549d0cb0fd68a310c0e88504f6037b42
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-04-03 19:50:01 +0330

    fix store to merchant

--- app/View/Components/MerchantLayout.php
+++ app/View/Components/MerchantLayout.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class StoreLayout extends Component
+class MerchantLayout extends Component
 {
     /**
      * Create a new component instance.
@@ -23,6 +23,6 @@ class StoreLayout extends Component
      */
     public function render()
     {
-        return view('layouts.store');
+        return view('layouts.merchant');
     }
 }
