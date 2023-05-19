a265a64335866056e7611b4e4a6e6b32ec36167b
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2023-05-19 19:33:54 +0330

    address transaction

---
+++ app/Console/Commands/BepAddressCheckerCommand.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Log;
+
+class BepAddressCheckerCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bep:address_checker';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $uri = 'https://bsc-dataseed1.defibit.io/';// Mainnet
+        $api = new \Binance\NodeApi($uri);
+        $bnb = new \Binance\Bnb($api);
+
+        $config = [
+            'contract_address' => '0x55d398326f99059fF775485246999027B3197955',// USDT BEP20
+            'decimals' => 18,
+        ];
+        $bep20 = new \Binance\BEP20($api, $config);
+
+
+        // *Check balances
+        $address = '0x450c4e9205c2ccd907ff6abddd63699168ff5749';
+        $bnb->bnbBalance($address);
+        echo $bep20->balance($address);
+
+
+        //$blockID = $bep20->blockNumber();
+
+        //$txHash = '0x565a370d5558140f55b638236325ef3fa38755cde4624095c9e5fa2898343510';
+        //var_dump($bep20->getTransactionReceipt($txHash));
+
+        //Log::info($bep20->getBlockByNumber(21609237));
+    }
+}
---
+++ app/Console/Commands/BnbAddressCheckerCommand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class BnbAddressCheckerCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'bnb:address_checker';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
